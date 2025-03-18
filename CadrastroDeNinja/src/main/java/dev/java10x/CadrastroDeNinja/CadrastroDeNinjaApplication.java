8bea85600a79b570951ee74e7c2e4082c8cc5126
Jeannfreitas <freitasjean344@gmail.com>
2025-03-18 03:05:06 -0300

    Atualizando .gitignore

---
+++ CadrastroDeNinja/src/main/java/dev/java10x/CadrastroDeNinja/CadrastroDeNinjaApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadrastroDeNinja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadrastroDeNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadrastroDeNinjaApplication.class, args);
+	}
+
+}
---
+++ CadrastroDeNinja/src/test/java/dev/java10x/CadrastroDeNinja/CadrastroDeNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadrastroDeNinja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadrastroDeNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
